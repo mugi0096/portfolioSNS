16c4dcf2e4333cec7c0eb2d59725281685452415
ue-tsu <ue-tsu@ue-tsu-0106-no-MacBook-Pro.local>
2020-05-08 22:05:13 +0900

    add data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,96 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Skill.create(:name => "Java")
+Skill.create(:name => "C")
+Skill.create(:name => "C++")
+Skill.create(:name => "C#")
+Skill.create(:name => "PHP")
+Skill.create(:name => "Ruby")
+Skill.create(:name => "Python")
+Skill.create(:name => "Perl")
+Skill.create(:name => "Objective-C")
+Skill.create(:name => "Swift")
+Skill.create(:name => "VB")
+Skill.create(:name => "ActionScript")
+Skill.create(:name => "R")
+Skill.create(:name => "Groovy")
+Skill.create(:name => "Haskell")
+Skill.create(:name => "Scala")
+Skill.create(:name => "Erlang")
+Skill.create(:name => "Go")
+Skill.create(:name => "JavaScript")
+Skill.create(:name => "HTML+CSS")
+Skill.create(:name => "CoffeeScript")
+Skill.create(:name => "Haml")
+Skill.create(:name => "Sass")
+Skill.create(:name => "TypeScript")
+Skill.create(:name => "SQL")
+Skill.create(:name => "PL/SQL")
+Skill.create(:name => "COBOL")
+Skill.create(:name => "ABAP")
+Skill.create(:name => "Kotlin")
+Skill.create(:name => "Rust")
+Skill.create(:name => "AHDL")
+Skill.create(:name => "Bash")
+Skill.create(:name => "Clojure")
+Skill.create(:name => "ClojureScript")
+Skill.create(:name => "Common Lisp")
+Skill.create(:name => "CUDA C/C++")
+Skill.create(:name => "ECMAScript")
+Skill.create(:name => "Elixir")
+Skill.create(:name => "F#")
+Skill.create(:name => "Lua")
+Skill.create(:name => "MATLAB")
+Skill.create(:name => "Nim")
+Skill.create(:name => "Objective Caml")
+Skill.create(:name => "Prolog")
+Skill.create(:name => "Scheme")
+Skill.create(:name => "Verilog HDL")
+Skill.create(:name => "VHDL")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
